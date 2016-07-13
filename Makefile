e3d157afa4c01ffa929ffd4a834396ccff148d9f
Liam Young <liam.young@canonical.com>
2016-07-13 14:02:28 +0000

    Add License info

--- Makefile
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/make
-LAYER_PATH := layers
-INTERFACE_PATH := interfaces
-
-clean:
-	rm -Rf build
-
-generate: clean
-	LAYER_PATH=$(LAYER_PATH) INTERFACE_PATH=$(INTERFACE_PATH) tox -e generate
--- src/reactive/designate_handlers.py
+++ src/reactive/designate_handlers.py
@@ -1,6 +1,21 @@
+# Copyright 2016 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import charm.openstack.designate as designate
 import charmhelpers.core.hookenv as hookenv
 import charms.reactive as reactive
-import charm.openstack.designate as designate
 
 COMPLETE_INTERFACE_STATES = [
     'dns-backend.available',
